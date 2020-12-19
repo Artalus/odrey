0b6f5b24205fa6dc9ef51fb8a82effe204d764ad
Artalus <artalus-mail@yandex.ru>
2020-12-19 13:38:44 +0300

    indent space=4

--- odr.py
+++ odr.py
@@ -7,10 +7,10 @@ from typing import List, Dict
 
 
 def parse_args() -> argparse.Namespace:
-   p = argparse.ArgumentParser()
-   p.add_argument('inputfiles', nargs='+')
-   p.add_argument('-Werror', action='store_true')
-   return p.parse_args()
+    p = argparse.ArgumentParser()
+    p.add_argument('inputfiles', nargs='+')
+    p.add_argument('-Werror', action='store_true')
+    return p.parse_args()
 
 
 #                            num      value   size    type    bind    vis     ndx     name
@@ -23,61 +23,61 @@ SizeInFile = namedtuple('SizeData', ['filename','size'])
 
 
 class FileData:
-   def __init__(self, filename: str, symbols: List[Symbol]):
-       self.filename = filename
-       self.symbols = symbols
+    def __init__(self, filename: str, symbols: List[Symbol]):
+        self.filename = filename
+        self.symbols = symbols
 
 
 class Collision:
-   def __init__(self, funcname: str, entries: List[SizeInFile]):
-       self.funcname = funcname
-       self.entries = entries
+    def __init__(self, funcname: str, entries: List[SizeInFile]):
+        self.funcname = funcname
+        self.entries = entries
 
 
 def main() -> None:
-   args = parse_args()
-   symbols = [read_symbols(f) for f in args.inputfiles]
-   collisions = find_collisions(symbols)
-   if collisions:
-       print('ODR violations found:')
-       for c in collisions:
-           print(collision_to_str(c))
-       if args.Werror:
-           exit(1)
+    args = parse_args()
+    symbols = [read_symbols(f) for f in args.inputfiles]
+    collisions = find_collisions(symbols)
+    if collisions:
+        print('ODR violations found:')
+        for c in collisions:
+            print(collision_to_str(c))
+        if args.Werror:
+            exit(1)
 
 
 def read_symbols(filename: str) -> FileData:
-   elf = subprocess.check_output(f'readelf -Ws {filename} | c++filt', shell=True).decode()
-   elf = elf.splitlines()
-   symbols = []
-   for line in elf:
-       m = re.match(SYMBOL_RE, line)
-       if m:
-           symbols.append(Symbol(*(m.groups())))
-   functions = [s for s in symbols if s.type == 'FUNC']
-   return FileData(filename, functions)
+    elf = subprocess.check_output(f'readelf -Ws {filename} | c++filt', shell=True).decode()
+    elf = elf.splitlines()
+    symbols = []
+    for line in elf:
+        m = re.match(SYMBOL_RE, line)
+        if m:
+            symbols.append(Symbol(*(m.groups())))
+    functions = [s for s in symbols if s.type == 'FUNC']
+    return FileData(filename, functions)
 
 
 def find_collisions(filesdata: List[FileData]) -> List[Collision]:
-   known_sizes: Dict[str, List[SizeInFile]] = dict()
-   for fd in filesdata:
-       for s in fd.symbols:
-           if s.name not in known_sizes:
-               known_sizes[s.name] = []
-           known_sizes[s.name].append(SizeInFile(fd.filename, s.size))
-   collisions = []
-   for funcname, sizedatas in known_sizes.items():
-       uniq = sizedatas[0].size
-       if any(sd.size != uniq for sd in sizedatas):
-           collisions.append(Collision(funcname, sizedatas))
-   return collisions
+    known_sizes: Dict[str, List[SizeInFile]] = dict()
+    for fd in filesdata:
+        for s in fd.symbols:
+            if s.name not in known_sizes:
+                known_sizes[s.name] = []
+            known_sizes[s.name].append(SizeInFile(fd.filename, s.size))
+    collisions = []
+    for funcname, sizedatas in known_sizes.items():
+        uniq = sizedatas[0].size
+        if any(sd.size != uniq for sd in sizedatas):
+            collisions.append(Collision(funcname, sizedatas))
+    return collisions
 
 
 def collision_to_str(c: Collision) -> str:
-   size_strings = [f'  in file {f}: size={sz}' for f,sz in c.entries]
-   ss = '\n'.join(size_strings)
-   return f'multiple definitions of {c.funcname}:\n{ss}'
+    size_strings = [f'  in file {f}: size={sz}' for f,sz in c.entries]
+    ss = '\n'.join(size_strings)
+    return f'multiple definitions of {c.funcname}:\n{ss}'
 
 
 if __name__ == "__main__":
-   main()
+    main()
