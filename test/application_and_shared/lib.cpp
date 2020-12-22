daab3699d6535534292520b0e6c46fe12a5de59a
Artalus <artalus-mail@yandex.ru>
2020-12-22 12:37:57 +0300

    Fix linking with shared library on windows

--- test/application_and_shared/lib.cpp
+++ test/application_and_shared/lib.cpp
@@ -1,13 +1,19 @@
+#include <application_and_shared-lib_export.h>
+
+    APPLICATION_AND_SHARED_LIB_EXPORT
 double colliding_variable = 11.11;
 
+    APPLICATION_AND_SHARED_LIB_EXPORT
 extern const double colliding_const = 12.12;
 
 double colliding_func(double d) { return d + colliding_variable + colliding_const; }
 
+    APPLICATION_AND_SHARED_LIB_EXPORT
 double uses_colliding_in_library(double d) { return colliding_func(d); }
 
 inline double colliding_inline(double d) { return 2.0 + d + colliding_variable + colliding_const; }
 
+    APPLICATION_AND_SHARED_LIB_EXPORT
 double uses_colliding_inline_in_library(double d) { return colliding_inline(d); }
 
 static double local_variable_1 = 13.13;
@@ -24,6 +30,7 @@ namespace {
     }
 }
 
+    APPLICATION_AND_SHARED_LIB_EXPORT
 int function_in_lib(int x) {
     return local_function_1(x) + local_function_2(x);
 }
--- test/application_and_shared/main.cpp
+++ test/application_and_shared/main.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
+#include <application_and_shared-lib_export.h>
 
 int function_in_app(int);
 double uses_colliding_in_app(double);
 double uses_colliding_inline_in_app(double);
 
+    APPLICATION_AND_SHARED_LIB_EXPORT
 int function_in_lib(int);
+    APPLICATION_AND_SHARED_LIB_EXPORT
 double uses_colliding_in_library(double);
+    APPLICATION_AND_SHARED_LIB_EXPORT
 double uses_colliding_inline_in_library(double);
 
+    APPLICATION_AND_SHARED_LIB_EXPORT
 extern short colliding_variable;
+    APPLICATION_AND_SHARED_LIB_EXPORT
 extern const short colliding_const;
 
 int main() {
