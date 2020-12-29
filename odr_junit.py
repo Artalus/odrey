de9e378bc2ca070c51d31e30fb11f07d63b54728
Artalus <artalus-mail@yandex.ru>
2020-12-29 11:08:26 +0300

    get rid of lxml, small fixes

--- odr_junit.py
+++ odr_junit.py
@@ -3,33 +3,38 @@
 import argparse
 import json
 import os
-import lxml.etree as ET
+from typing import *
 
+from xml.etree import ElementTree as ET
+from xml.dom import minidom
 
-def parse_args():
+Json = Dict[str, Any]
+
+def parse_args() -> argparse.Namespace:
     p = argparse.ArgumentParser()
     p.add_argument('input_dir')
     p.add_argument('output_file')
+    p.add_argument('--json_extension', default='json')
     return p.parse_args()
 
 
-def main():
+def main() -> None:
     args = parse_args()
-    jsons = collect_jsons(args.input_dir)
+    jsons = collect_jsons(args.input_dir, args.json_extension)
     x = json_to_xml(jsons)
     write_xml(x, args.output_file)
 
 
-def collect_jsons(input_dir):
+def collect_jsons(input_dir: str, file_extension: str) -> List[Json]:
     jsons = []
     for dirpath, _, files in os.walk(input_dir):
         for f in files:
-            if f.endswith('.odr.json'):
-                with open(f'{dirpath}/{f}') as fp:
+            if f.endswith(file_extension):
+                with open(os.path.join(dirpath, f)) as fp:
                     jsons.append(json.load(fp))
     return jsons
 
-def json_to_xml(jsons):
+def json_to_xml(jsons: List[Json]) -> ET.Element:
     '''
 <testsuite tests="3">
     <testcase classname="foo1" name="ASuccessfulTest"/>
@@ -74,12 +79,12 @@ def json_to_xml(jsons):
             case = ET.SubElement(suite, 'testcase')
             case.set('name', tname)
 
-            files = ['%s size=%s' % (x['file'], x['size']) for x in c['entries']]
+            files = ['%s: %s' % (x['filename'], x['data']) for x in c['entries']]
             files_str = '\n'.join(files)
 
             failure = ET.SubElement(case, 'failure')
             failure.set('type', 'OdrViolation')
-            failure.text = f'CMake target: {tname}\nFunction {fname} definition differs in files:\n{files_str}'
+            failure.text = f'CMake target: {tname}\nSymbol `{fname}` definition differs in files:\n{files_str}'
 
             serr = ET.SubElement(case, 'system-err')
             jdump = json.dumps(c['entries'], indent=2)
@@ -87,10 +92,17 @@ def json_to_xml(jsons):
     return root
 
 
-def write_xml(xmldata, filename):
-    xmlstr = ET.tostring(xmldata, pretty_print=True)
-    with open(filename, "wb") as f:
+def write_xml(xmldata: ET.Element, filename: str) -> None:
+    xmlstr = prety_xml(xmldata)
+    with open(filename, "w") as f:
         f.write(xmlstr)
+
+
+# silly hack to get indented xml without LXML package
+def prety_xml(elem: ET.Element) -> str:
+    rough_string = ET.tostring(elem, 'utf-8')
+    reparsed = minidom.parseString(rough_string)
+    return reparsed.toprettyxml(indent="    ") # type: ignore
 
 
 if __name__ == "__main__":
